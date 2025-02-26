297fa9e038686521c3fea861ec6389b0fd82181d
burak balcı <burak.balci@windowslive.com>
2025-02-27 00:27:52 +0300

    prop hatası çözüldü

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,14 +1,13 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link, Outlet } from "react-router-dom";
 import ProfileCard from "../components/ProfileCard";
 const ProfilePage = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
   const handleUpdate = () => {
     alert("Update button clicked!");
   };
   return (
     <div className="d-flex justify-content-center mt-5">
       <ProfileCard
-        email="nightWhisper.com"
+        email={user.email}
         password="********"
         onUpdate={handleUpdate}
       />
